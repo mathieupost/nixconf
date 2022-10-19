c85a040da7195f3e340a629efc969b18bde78e52
Mathieu Post <mathieupost@gmail.com>
2022-10-19 20:14:52 +0200

    nix: use new darwin settings for build users and optimise-store

--- configuration.nix
+++ configuration.nix
@@ -5,10 +5,10 @@
   # Enable experimental nix command and flakes
   # nix.package = pkgs.nixUnstable;
   nix.extraOptions = ''
-    auto-optimise-store = true
     experimental-features = nix-command flakes
   '';
-  users.nix.configureBuildUsers = true;
+  nix.configureBuildUsers = true;
+  nix.settings.auto-optimise-store = true;
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
