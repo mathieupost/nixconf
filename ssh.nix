00cbbbc274f05a93c393388f0da472d37a7d2eac
Mathieu Post <mathieupost@gmail.com>
2022-12-22 16:39:27 +0100

    ssh: remove old host blocks

--- ssh.nix
+++ ssh.nix
@@ -3,16 +3,6 @@
 {
   programs.ssh = {
     enable = true;
-    matchBlocks = {
-      versio = {
-        hostname = "vserver90.axc.eu";
-        user = "mathikx90";
-      };
-      satellite = {
-        hostname = "192.168.178.90";
-        user = "mathieu";
-      };
-    };
     extraOptionOverrides = {
       IdentityFile = "~/.ssh/id_rsa";
       AddKeysToAgent = "yes";
