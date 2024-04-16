91c06aad89cb2bc842a11406901fd1d22cfd6967
Mathieu Post <mathieupost@gmail.com>
2024-04-16 11:52:40 +0200

    chore: remove direnv layout_poetry

--- shell.nix
+++ shell.nix
@@ -165,31 +165,6 @@
     nix-direnv.enable = true;
     stdlib = ''
       tmutil addexclusion $(direnv_layout_dir)
-
-      layout_poetry() {
-        export POETRY_VIRTUALENVS_PATH="$(direnv_layout_dir)/poetry"
-        PYPROJECT_TOML="''${PYPROJECT_TOML:-pyproject.toml}"
-        if [[ ! -f "$PYPROJECT_TOML" ]]; then
-            log_status "No pyproject.toml found. Executing \`poetry init\` to create a \`$PYPROJECT_TOML\` first."
-            poetry init
-        fi
-
-        if [[ -d ".venv" ]]; then
-            VIRTUAL_ENV="$(pwd)/.venv"
-        else
-            VIRTUAL_ENV=$(poetry env info --path 2>/dev/null ; true)
-        fi
-
-        if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
-            log_status "No virtual environment exists. Executing \`poetry install\` to create one."    
-            poetry install
-            VIRTUAL_ENV=$(poetry env info --path)
-        fi
-
-        PATH_add "$VIRTUAL_ENV/bin"
-        export POETRY_ACTIVE=1
-        export VIRTUAL_ENV
-      }
     '';
   };
   programs.fzf = {
